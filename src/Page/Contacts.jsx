cf190fe612d173735f20604ba010716b43009aac
leroyiq <leroyiq6@gmail.com>
2024-08-21 13:17:54 +0300

    Update Contacts.jsx

--- src/Page/Contacts.jsx
+++ src/Page/Contacts.jsx
@@ -23,7 +23,7 @@ export const Contacts = () => {
           </a>
         </li>
         <li className="contactsItem">
-          <a href="https://t.me/dmitro_kv">
+          <a href="https://t.me/G_f_n_u">
             <FaTelegram className="contactIcon" />
             Telegram
           </a>
